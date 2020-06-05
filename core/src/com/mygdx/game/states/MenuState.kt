dda17e9ee35387d5958884b1ec26dd6723963b31
Fernando Monteiro <fernando.vicente.monteiro@usp.br>
2020-06-04 23:53:52 -0300

    Aula 4: Menu Screen

--- core/src/com/mygdx/game/states/MenuState.kt
+++ core/src/com/mygdx/game/states/MenuState.kt
@@ -1,8 +1,13 @@
 package com.mygdx.game.states
 
+import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.mygdx.game.FlappyDemo
 
 class MenuState(gsm: GameStateManager) : State(gsm) {
+    private val background: Texture = Texture("bg.png")
+    private val playBtn: Texture = Texture("playBtn.png")
+
     override fun handleInput() {
 //        TODO("Not yet implemented")
     }
@@ -11,7 +16,20 @@ class MenuState(gsm: GameStateManager) : State(gsm) {
 //        TODO("Not yet implemented")
     }
 
+    // O SpriteBatch funciona como uma caixa,
+    // abrimos a caixa, colocamos o que queremos
+    // renderizar e fechamos a caixa novamente
     override fun render(sb: SpriteBatch) {
-//        TODO("Not yet implemented")
+        // Abrindo a caixa
+        sb.begin()
+
+        // Colocando tudo dentro
+        sb.draw(background, 0.toFloat(), 0.toFloat(),
+                FlappyDemo.WIDTH.toFloat(), FlappyDemo.HEITGHT.toFloat())
+        sb.draw(playBtn, ((FlappyDemo.WIDTH / 2) - (playBtn.width / 2)).toFloat(),
+                (FlappyDemo.HEITGHT / 2).toFloat())
+
+        // Fechando a caixa
+        sb.end()
     }
 }
